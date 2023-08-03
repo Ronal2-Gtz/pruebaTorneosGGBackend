1b5920a961c6563466a30c78e52067e06a06db76
Ronaldo Gutierrez <ronal2.gutierrez@gmail.comgit config --global user.email ronal2.gutierrez@gmail.com>
2023-08-03 10:27:20 -0500

    Config remote DB

--- src/db/config.ts
+++ src/db/config.ts
@@ -5,11 +5,12 @@ import { User } from '../entities/User';
 
 const AppDataSource = new DataSource({
 	type: 'postgres',
-	host: 'localhost',
-	username: 'postgres',
+	host: 'ep-damp-band-05825782.us-east-2.aws.neon.tech',
+	username: 'fl0user',
 	password: '230691Ro',
 	port: 5432,
 	database: 'news',
+	url: 'postgres://fl0user:a8KUWYriO3bw@ep-damp-band-05825782.us-east-2.aws.neon.tech:5432/news?sslmode=require',
 	entities: [Commentary, News, User],
 
 	synchronize: true,
